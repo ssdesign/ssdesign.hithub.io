5da84c7f77224de1d099dcc2e93fe1dd6df6cf68
ssdesign <sajid.saiyed@gmail.com>
2020-08-14 19:09:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -96,58 +96,6 @@ function touchHandler(ev){
     e.preventDefault()
   }
 }
-
-function touchHandler(ev) {
-  console.log("======"+xp);
-  if(mouseStatus == "down"){
-    console.log(xp);
-    if(ev) {
-      var touchobj = ev.changedTouches[0]
-        //var deltaX = ev.touches[0].pageX - x;
-        //  var deltaY = ev.touches[0].pageY - y;
-
-        var deltaX = parseInt(touchobj.clientX) - xp;
-        var deltaY = parseInt(touchobj.clientY) - yp;
-
-
-        //var deltaX = ev.clientX-xp;
-        //var deltaY = ev.clientY-yp;
-        //xdisp = xp+deltaX;
-        //ydisp = yp+deltaY;
-
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(base_image, deltaX, deltaY);
-        xp = deltaX;
-        yp = deltaY;
-        ev.preventDefault();
-}
-    if(ev) {
-      deltaX = ev.clientX-x;
-      deltaY = ev.clientY-y;
-      xdisp = deltaX+xp;
-      ydisp = deltaY+yp;
-
-      if(xp >= canvas.width-50 || xp <= 0 || yp >= canvas.height-50 || yp<=0 ){
-        x=ev.clientX;
-        y=ev.clientY;
-        //xp=base_image.x;
-        //yp=base_image.y;
-      }
-      else{
-
-        xp = xdisp;
-        yp = ydisp;
-        x=ev.clientX;
-        y=ev.clientY;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(base_image, xdisp, ydisp);
-        ev.preventDefault();
-      }
-
-
-  }
-}
-}
 function setDown(ev){
   mouseStatus = "down";
   x = ev.clientX
